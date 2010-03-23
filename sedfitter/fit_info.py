Bug fix for fit number selecting function Fix bug with upper/lower limits chi^2 calculation Fix bug with order of SEDs in plots (black was last not first) Fixed bug with aperture interpolation New function to extract parameter values from fits

sedfitter/fit_info.py
@@ -11,6 +11,7 @@ class FitInfo(object):
         self.sc = None
         self.chi2 = None
         self.model_id = None
+        self.model_name = None
 
         return
 
@@ -20,6 +21,7 @@ class FitInfo(object):
         self.av = self.av[order]
         self.sc = self.sc[order]
         self.chi2 = self.chi2[order]
+        self.model_name = self.model_name[order]
         self.model_id = order
 
         return
@@ -31,26 +33,27 @@ class FitInfo(object):
         elif form=='N' or not form: # Form N is parsed as boolean
             n_fits = int(number)
         elif form=='C':
-            n_fits = len(self.chi2 <= number)
+            n_fits = np.sum(self.chi2 <= number)
         elif form=='D':
-            n_fits = len(self.chi2 - self.chi2[0] <= number)
+            n_fits = np.sum(self.chi2 - self.chi2[0] <= number)
         elif form=='E':
-            n_fits = len((self.chi2 / self.source.n_wav) <= number)
+            n_fits = np.sum((self.chi2 / self.source.n_wav) <= number)
         elif form=='F':
-            n_fits = len((self.chi2 - self.chi2[0]) / self.source.n_wav <= number)
+            n_fits = np.sum((self.chi2 - self.chi2[0]) / self.source.n_wav <= number)
         else:
             raise Exception("Unknown format: %s" % form)
 
         self.av = self.av[:n_fits]
         self.sc = self.sc[:n_fits]
         self.chi2 = self.chi2[:n_fits]
+        self.model_name = self.model_name[:n_fits]
         self.model_id = self.model_id[:n_fits]
 
         return
 
     def __getattr__(self, attribute):
         if attribute == 'n_fits':
-            return len(self.model_id)
+            return len(self.chi2)
         else:
             raise AttributeError(attribute)
 
@@ -61,6 +64,8 @@ class FitInfo(object):
         file_handle.write(self.sc.astype(np.float32).tostring())
         file_handle.write(self.chi2.astype(np.float32).tostring())
         file_handle.write(self.model_id.astype(np.int32).tostring())
+        pickle.dump(self.model_name.itemsize, file_handle, 2)
+        file_handle.write(self.model_name.tostring())
 
     def read_binary(self, file_handle):
         self.source = Source()
@@ -70,3 +75,5 @@ class FitInfo(object):
         self.sc = np.fromstring(file_handle.read(n_fits*4), dtype=np.float32)
         self.chi2 = np.fromstring(file_handle.read(n_fits*4), dtype=np.float32)
         self.model_id = np.fromstring(file_handle.read(n_fits*4), dtype=np.int32)
+        itemsize = pickle.load(file_handle)
+        self.model_name = np.fromstring(file_handle.read(n_fits*itemsize), dtype='|S%i' % itemsize)

sedfitter/fitting_routines.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def linear_regression(data, weights, pattern1, pattern2):
 
     c1 = np.sum(data*pattern1*weights, axis=1)
@@ -19,9 +20,10 @@ def linear_regression(data, weights, pattern1, pattern2):
 def optimal_scaling(data, weights, pattern1):
     return np.sum(data*pattern1*weights, axis=data.ndim-1) / np.sum(pattern1*pattern1*weights)
 
+
 def chi_squared(valid, data, error, weight, model):
     '''
-    valid has dimension (n_wav,)
+    valid has dimension (n_wav, )
     data has dimension (n_wav, n_models)
     error has dimension(n_wav)
     weight has dimension(n_wav)
@@ -40,20 +42,24 @@ def chi_squared(valid, data, error, weight, model):
         raise Exception("Chi^2 array has unexpected number of dimensions: %i" % chi2_array.ndim)
 
     # Reset lower limits where model < data
-    reset = model < data
     if chi2_array.ndim == 2:
-        reset[:, valid<>2] = False
+        for j in np.where(valid==2):
+            reset = model[:, j] < data[:, j]
+            chi2_array[:, j][reset] == -2. * np.log10(1.-error[j])
     else:
-        reset[:, :, valid<>2] = False
-    chi2_array[reset] = -2. * np.log10(1.-error[valid==2])
+        for j in np.where(valid==2):
+            reset = model[:, :, j] < data[:, :, j]
+            chi2_array[:, :, j][reset] == -2. * np.log10(1.-error[j])
 
     # Reset upper limits where model > data
-    reset = model > data
     if chi2_array.ndim == 2:
-        reset[:, valid<>3] = False
+        for j in np.where(valid==3):
+            reset = model[:, j] > data[:, j]
+            chi2_array[:, j][reset] == -2. * np.log10(1.-error[j])
     else:
-        reset[:, :, valid<>3] = False
-    chi2_array[reset] = -2. * np.log10(1.-error[valid==3])
+        for j in np.where(valid==3):
+            reset = model[:, :, j] > data[:, :, j]
+            chi2_array[:, :, j][reset] == -2. * np.log10(1.-error[j])
 
     # Check that there are no infinities
     chi2_array[np.isinf(chi2_array)] = 1.e30

sedfitter/models.py
@@ -94,7 +94,7 @@ class Models(object):
         else:
             self.fluxes = np.column_stack(model_fluxes)
 
-        self.names = conv.model_names
+        self.names = np.char.strip(conv.model_names)
         self.n_models = conv.n_models
 
         self.valid = self.fluxes <> 0
@@ -159,4 +159,4 @@ class Models(object):
 
             raise Exception("Unexpected number of dimensions in flux array")
 
-        return av_best, sc_best, ch_best
+        return av_best, sc_best, ch_best, self.names

sedfitter/plot.py
@@ -79,7 +79,7 @@ color['faded'].append((0.50, 0.90, 0.50))
 def tex_friendly(string):
     return string.replace('_','\_').replace('%','\%')
 
-def plot_source_info(ax, i, info, model_name, plot_name, plot_info):
+def plot_source_info(ax, i, info, plot_name, plot_info):
 
     labels = []
 
@@ -87,7 +87,7 @@ def plot_source_info(ax, i, info, model_name, plot_name, plot_info):
         labels.append(tex_friendly(info.source.name))
 
     if plot_info:
-        labels.append("Model: %s" % tex_friendly(model_name))
+        labels.append("Model: %s" % tex_friendly(info.model_name[i]))
         if i==0:
             labels.append("Best fit")
         else:
@@ -172,7 +172,6 @@ def plot(input_file, output_dir, select_format=("N", 1), plot_mode="A", sed_type
 
     model_dir = pickle.load(fin)
     filters = pickle.load(fin)
-    model_names = pickle.load(fin)
     extinction = Extinction()
     extinction.read_binary(fin)
 
@@ -202,13 +201,13 @@ def plot(input_file, output_dir, select_format=("N", 1), plot_mode="A", sed_type
         lines = []
         colors = []
 
-        for i in range(info.n_fits):
+        for i in range(info.n_fits-1,-1,-1):
 
-            if (plot_mode == 'A' and i == 0) or plot_mode == 'I':
+            if (plot_mode == 'A' and i == info.n_fits-1) or plot_mode == 'I':
                 fig = mpl.figure()
                 ax = get_axes(fig)
 
-            if (plot_mode == 'A' and i == info.n_fits - 1) or plot_mode == 'I':
+            if (plot_mode == 'A' and i == 0) or plot_mode == 'I':
                 if sed_type in ['interp', 'largest']:
                     color_type = 'black'
                 else:
@@ -219,13 +218,11 @@ def plot(input_file, output_dir, select_format=("N", 1), plot_mode="A", sed_type
                 else:
                     color_type = 'faded'
 
-            model_name = model_names[info.model_id[i]].strip()
-
             s = SED()
             if modpar['length_subdir'] == 0:
-                s.read(model_dir + '/seds/' + model_name + '_sed.fits')
+                s.read(model_dir + '/seds/' + info.model_name[i] + '_sed.fits')
             else:
-                s.read(model_dir + '/seds/%s/%s_sed.fits' % (model_name[:modpar['length_subdir']], model_name))
+                s.read(model_dir + '/seds/%s/%s_sed.fits' % (info.model_name[i][:modpar['length_subdir']], info.model_name[i]))
 
             s.scale_to_distance(10.**info.sc[i])
             s.scale_to_av(info.av[i], extinction.av)
@@ -251,12 +248,16 @@ def plot(input_file, output_dir, select_format=("N", 1), plot_mode="A", sed_type
                 lines.append(np.column_stack([s.wav, flux]))
                 colors.append(color[color_type])
 
-            if (plot_mode == 'A' and i == info.n_fits-1) or plot_mode == 'I':
+            if (plot_mode == 'A' and i == 0) or plot_mode == 'I':
 
                 ax.add_collection(LineCollection(lines, colors=colors))
 
                 ax = plot_source_data(ax, info.source, filters)
-                ax = plot_source_info(ax, i, info, model_name, plot_name, plot_info)
+
+                if plot_mode == 'A':
+                    ax = plot_source_info(ax, 0, info, plot_name, plot_info)
+                else:
+                    ax = plot_source_info(ax, i, info, plot_name, plot_info)
 
                 ax.set_xlabel('$\lambda$ ($\mu$m)')
                 ax.set_ylabel('$\lambda$F$_\lambda$ (ergs/cm$^2$/s)')

sedfitter/sed.py
@@ -142,8 +142,8 @@ class SED(object):
         apertures = 10.**log10_ap_interp(np.log10(self.wav))
 
         # Extrapolate on either side
-        apertures[np.log10(self.wav) < log10_ap_interp.x[0]] = self.ap[0]
-        apertures[np.log10(self.wav) > log10_ap_interp.x[-1]] = self.ap[-1]
+        apertures[np.log10(self.wav) < log10_ap_interp.x[0]] = 10.**log10_ap_interp.y[0]
+        apertures[np.log10(self.wav) > log10_ap_interp.x[-1]] = 10.**log10_ap_interp.y[-1]
 
         # Interpolate and return only diagonal elements
         return flux_interp(apertures).diagonal()
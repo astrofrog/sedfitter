Fixed errors with Filter class and units

sedfitter/filter/filter.py
@@ -6,22 +6,22 @@ import numpy as np
 from astropy import units as u
 
 from ..utils.integrate import integrate_subset, integrate
+from ..utils.validator import validate_array, validate_scalar
 
 c = 299792458.
 
 
 class Filter(object):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         self.name = None
         self.wavelength = None
         self.wav = None
         self.nu = None
         self.r = None
-        if len(args) > 0:
-            self.read(*args, **kwargs)
 
-    def read(self, filename):
+    @classmethod
+    def read(cls, filename):
         """
         Read a filter from a file
 
@@ -31,19 +31,23 @@ class Filter(object):
             The name of the file containing the filter
         """
 
+        self = cls()
+
         # Read in central wavelength
-        self.wavelength = float(open(filename, 'r').readline().split('=')[1])
+        self.wavelength = float(open(filename, 'r').readline().split('=')[1]) * u.micron
 
         # Read in spectral response curve
-        self.wav = np.loadtxt(filename, usecols=[0], dtype=float)
+        self.wav = np.loadtxt(filename, usecols=[0], dtype=float) * u.micron
         self.r = np.loadtxt(filename, usecols=[1], dtype=float)
 
         # Compute frequency
-        self.nu = c / (self.wav * 1.e-6)
+        self.nu = self.wav.to(u.Hz, equivalencies=u.spectral())
 
         # Set name
         if self.name is None:
             self.name = os.path.basename(filename).split('.')[0]
+
+        return self
 
     def normalize(self):
         """
@@ -99,3 +103,72 @@ class Filter(object):
                 f.r[i] = integrate_subset(self_nu_hz, self.r, nu1, nu2)
 
         return f
+
+    @property
+    def wavelength(self):
+        """
+        The central or characteristic wavelength of the filter
+        """
+        return self._wavelength
+
+    @wavelength.setter
+    def wavelength(self, value):
+        if value is None:
+            self._wavelength = None
+        else:
+            if isinstance(value, u.Quantity) and value.unit.is_equivalent(u.m):
+                if not value.isscalar:
+                    raise TypeError("wavelength should be a scalar Quantity")
+                if not value > 0 * u.micron:
+                    raise ValueError("wavelength should be strictly positive")
+                self._wavelength = value
+            else:
+                raise TypeError("central wavelength should be given as a Quantity object with units of distance")
+
+    @property
+    def wav(self):
+        """
+        The wavelengths at which the filter is defined
+        """
+        return self._wav
+
+    @wav.setter
+    def wav(self, value):
+        if value is None:
+            self._wav = None
+        else:
+            if isinstance(value, u.Quantity) and value.unit.is_equivalent(u.m):
+                self._wav = validate_array('wav', value, domain='strictly-positive', ndim=1, shape=None if self.nu is None else (len(self.nu),))
+            else:
+                raise TypeError("wavelengths should be given as a Quantity object with units of distance")
+
+    @property
+    def nu(self):
+        """
+        The frequencies at which the filter is defined
+        """
+        return self._nu
+
+    @nu.setter
+    def nu(self, value):
+        if value is None:
+            self._nu = None
+        else:
+            if isinstance(value, u.Quantity) and value.unit.is_equivalent(u.Hz):
+                self._nu = validate_array('nu', value, domain='strictly-positive', ndim=1, shape=None if self.wav is None else (len(self.wav),))
+            else:
+                raise TypeError("frequencies should be given as a Quantity object with units of frequency")
+
+    @property
+    def r(self):
+        """
+        The filter response
+        """
+        return self._r
+
+    @r.setter
+    def r(self, value):
+        if value is None:
+            self._r = None
+        else:
+            self._r = validate_array('r', value, domain='positive', ndim=1, shape=None if (self.wav or self.nu) is None else (len(self.wav or self.nu),))

sedfitter/tests/test_full_pipeline.py
@@ -84,7 +84,7 @@ class BasePipelineTest(object):
 
         f1 = Filter()
         f1.name = 'alice'
-        f1.wavelength = 7.
+        f1.wavelength = 7. * u.micron
         f1.wav = np.linspace(5., 1., 100) * u.micron
         f1.nu = f1.wav.to(u.Hz, equivalencies=u.spectral())
         f1.r = np.random.random(100)
@@ -92,7 +92,7 @@ class BasePipelineTest(object):
 
         f2 = Filter()
         f2.name = 'bob'
-        f2.wavelength = 12.
+        f2.wavelength = 12. * u.micron
         f2.wav = np.linspace(15., 10., 100) * u.micron
         f2.nu = f2.wav.to(u.Hz, equivalencies=u.spectral())
         f2.r = np.random.random(100)
@@ -100,7 +100,7 @@ class BasePipelineTest(object):
 
         f3 = Filter()
         f3.name = 'eve'
-        f3.wavelength = 20.
+        f3.wavelength = 20. * u.micron
         f3.wav = np.linspace(25., 15., 100) * u.micron
         f3.nu = f3.wav.to(u.Hz, equivalencies=u.spectral())
         f3.r = np.random.random(100)
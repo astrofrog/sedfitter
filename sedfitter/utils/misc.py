87d96cefd58cbc6df7d739c23dfc9286005b85b8
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-10-15 15:50:53 +0200

    Properly test and sort models after convolution

---
+++ sedfitter/utils/misc.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def order_to_match(array, reference):
+    """
+    Given an array ``array``, return the index array needed to make it the same as ``reference``
+    """
+    return np.argsort(array)[np.argsort(np.argsort(reference))]

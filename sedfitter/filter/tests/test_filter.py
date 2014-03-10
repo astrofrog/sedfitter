32a695f4a87ca28bb675099bffb2a45802d5dcbb
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-03-10 14:39:48 +0100

    Fixed Filter tests

--- sedfitter/filter/tests/test_filter.py
+++ sedfitter/filter/tests/test_filter.py
@@ -14,41 +14,41 @@ def test_init():
     Filter()
 
 
-def test_wav():
+def test_nu():
     f = Filter()
-    f.wav = [1,2,3] * u.m
+    f.nu = [1,2,3] * u.Hz
 
 
-@pytest.mark.parametrize('value', [1. * u.m, [[1,2,3],[4,5,6]] * u.m])
-def test_wav_invalid_shape(value):
+@pytest.mark.parametrize('value', [1. * u.Hz, [[1,2,3],[4,5,6]] * u.Hz])
+def test_nu_invalid_shape(value):
     f = Filter()
     with pytest.raises(TypeError) as exc:
-        f.wav = value
-    assert exc.value.args[0] == 'wav should be a 1-d sequence'
+        f.nu = value
+    assert exc.value.args[0] == 'nu should be a 1-d sequence'
 
 
 @pytest.mark.parametrize('value', ['string', object(), 1., [1,2,3]])
-def test_wav_invalid_type(value):
+def test_nu_invalid_type(value):
     f = Filter()
     with pytest.raises(TypeError) as exc:
-        f.wav = value
-    assert exc.value.args[0] == 'wav should be given as a Quantity object'
+        f.nu = value
+    assert exc.value.args[0] == 'nu should be given as a Quantity object'
 
 
 @pytest.mark.parametrize('unit', [u.arcsec, u.kg, u.Jy])
-def test_wav_invalid_unit(unit):
+def test_nu_invalid_unit(unit):
     f = Filter()
     with pytest.raises(TypeError) as exc:
-        f.wav = [1., 2., 3.] * unit
-    assert exc.value.args[0] == 'wav should be given in units of length'
+        f.nu = [1., 2., 3.] * unit
+    assert exc.value.args[0] == 'nu should be given in units of frequency'
 
 
 @pytest.mark.parametrize('filename', ['2J.txt', 'I1.txt', 'S3.txt'])
 def test_read(filename):
     f = Filter.read(os.path.join(ROOT, 'data', filename))
     if filename == '2J.txt':
-        assert len(f.wav) == 107
+        assert len(f.nu) == 107
     elif filename == 'I1.txt':
-        assert len(f.wav) == 391
+        assert len(f.nu) == 391
     else:
-        assert len(f.wav) == 21
+        assert len(f.nu) == 21

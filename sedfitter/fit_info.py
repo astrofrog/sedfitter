Fixed a bug in the calculation of the reduced chi^2: this should have used the number of valid data points (valid flag 1 or 4) but instead was using the total number of valid and invalid points.

sedfitter/fit_info.py
@@ -159,9 +159,9 @@ class FitInfo(object):
         elif form == 'D':
             n_fits = np.sum(self.chi2 - self.chi2[0] <= number)
         elif form == 'E':
-            n_fits = np.sum((self.chi2 / self.source.n_wav) <= number)
+            n_fits = np.sum((self.chi2 / self.source.n_data) <= number)
         elif form == 'F':
-            n_fits = np.sum((self.chi2 - self.chi2[0]) / self.source.n_wav <= number)
+            n_fits = np.sum((self.chi2 - self.chi2[0]) / self.source.n_data <= number)
         else:
             raise Exception("Unknown format: %s" % form)
 